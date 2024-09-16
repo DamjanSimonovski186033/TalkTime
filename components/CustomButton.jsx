17d6c1c1ed20665fd9c52af83026e26975dc50e0
Zlatko Jovanovski <zlatko.jovanovski@students.finki.ukim.mk>
2024-09-16 13:21:56 +0200

    made sign-in and sign-up pages

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -6,7 +6,7 @@ const CustomButton = ({ title, handlePress, containerStyles, textStyles, isLoadi
     <TouchableOpacity 
         onPress={handlePress}
         activeOpacity={0.7}
-        className={'bg-[#FDFCED] rounded-[30%] min-h-[62px] w-[100%] justify-center items-center ${containerStyles} ${isLoading ? "opacity-50" : ""'}
+        className={'bg-[#FDFCED] rounded-2xl min-h-[62px] w-[100%] justify-center items-center ${containerStyles} ${isLoading ? "opacity-50" : ""'}
         disabled={isLoading}>
         <Text className="text-[#131217] font-psemibold text-lg">
             {title}
